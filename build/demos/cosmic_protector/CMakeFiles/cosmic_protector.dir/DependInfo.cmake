edbb886df5b0e869f9c39de62054f6eb02ca1605
phrasz <phrasz@github.com>
2013-06-16 15:03:35 -0400

    Build the source; cd build; sudo make install; x64 ...FYI

---
+++ build/demos/cosmic_protector/CMakeFiles/cosmic_protector.dir/DependInfo.cmake
@@ -0,0 +1,64 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/demos/cosmic_protector/src/Asteroid.cpp" "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/build/demos/cosmic_protector/CMakeFiles/cosmic_protector.dir/src/Asteroid.cpp.o"
+  "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/demos/cosmic_protector/src/BitmapResource.cpp" "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/build/demos/cosmic_protector/CMakeFiles/cosmic_protector.dir/src/BitmapResource.cpp.o"
+  "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/demos/cosmic_protector/src/Bullet.cpp" "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/build/demos/cosmic_protector/CMakeFiles/cosmic_protector.dir/src/Bullet.cpp.o"
+  "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/demos/cosmic_protector/src/ButtonWidget.cpp" "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/build/demos/cosmic_protector/CMakeFiles/cosmic_protector.dir/src/ButtonWidget.cpp.o"
+  "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/demos/cosmic_protector/src/Debug.cpp" "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/build/demos/cosmic_protector/CMakeFiles/cosmic_protector.dir/src/Debug.cpp.o"
+  "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/demos/cosmic_protector/src/DisplayResource.cpp" "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/build/demos/cosmic_protector/CMakeFiles/cosmic_protector.dir/src/DisplayResource.cpp.o"
+  "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/demos/cosmic_protector/src/Entity.cpp" "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/build/demos/cosmic_protector/CMakeFiles/cosmic_protector.dir/src/Entity.cpp.o"
+  "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/demos/cosmic_protector/src/Explosion.cpp" "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/build/demos/cosmic_protector/CMakeFiles/cosmic_protector.dir/src/Explosion.cpp.o"
+  "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/demos/cosmic_protector/src/FontResource.cpp" "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/build/demos/cosmic_protector/CMakeFiles/cosmic_protector.dir/src/FontResource.cpp.o"
+  "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/demos/cosmic_protector/src/GUI.cpp" "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/build/demos/cosmic_protector/CMakeFiles/cosmic_protector.dir/src/GUI.cpp.o"
+  "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/demos/cosmic_protector/src/Game.cpp" "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/build/demos/cosmic_protector/CMakeFiles/cosmic_protector.dir/src/Game.cpp.o"
+  "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/demos/cosmic_protector/src/Input.cpp" "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/build/demos/cosmic_protector/CMakeFiles/cosmic_protector.dir/src/Input.cpp.o"
+  "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/demos/cosmic_protector/src/LargeAsteroid.cpp" "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/build/demos/cosmic_protector/CMakeFiles/cosmic_protector.dir/src/LargeAsteroid.cpp.o"
+  "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/demos/cosmic_protector/src/LargeBullet.cpp" "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/build/demos/cosmic_protector/CMakeFiles/cosmic_protector.dir/src/LargeBullet.cpp.o"
+  "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/demos/cosmic_protector/src/LargeSlowBullet.cpp" "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/build/demos/cosmic_protector/CMakeFiles/cosmic_protector.dir/src/LargeSlowBullet.cpp.o"
+  "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/demos/cosmic_protector/src/MediumAsteroid.cpp" "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/build/demos/cosmic_protector/CMakeFiles/cosmic_protector.dir/src/MediumAsteroid.cpp.o"
+  "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/demos/cosmic_protector/src/Player.cpp" "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/build/demos/cosmic_protector/CMakeFiles/cosmic_protector.dir/src/Player.cpp.o"
+  "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/demos/cosmic_protector/src/PowerUp.cpp" "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/build/demos/cosmic_protector/CMakeFiles/cosmic_protector.dir/src/PowerUp.cpp.o"
+  "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/demos/cosmic_protector/src/Resource.cpp" "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/build/demos/cosmic_protector/CMakeFiles/cosmic_protector.dir/src/Resource.cpp.o"
+  "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/demos/cosmic_protector/src/ResourceManager.cpp" "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/build/demos/cosmic_protector/CMakeFiles/cosmic_protector.dir/src/ResourceManager.cpp.o"
+  "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/demos/cosmic_protector/src/SampleResource.cpp" "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/build/demos/cosmic_protector/CMakeFiles/cosmic_protector.dir/src/SampleResource.cpp.o"
+  "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/demos/cosmic_protector/src/SmallAsteroid.cpp" "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/build/demos/cosmic_protector/CMakeFiles/cosmic_protector.dir/src/SmallAsteroid.cpp.o"
+  "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/demos/cosmic_protector/src/SmallBullet.cpp" "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/build/demos/cosmic_protector/CMakeFiles/cosmic_protector.dir/src/SmallBullet.cpp.o"
+  "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/demos/cosmic_protector/src/StreamResource.cpp" "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/build/demos/cosmic_protector/CMakeFiles/cosmic_protector.dir/src/StreamResource.cpp.o"
+  "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/demos/cosmic_protector/src/UFO.cpp" "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/build/demos/cosmic_protector/CMakeFiles/cosmic_protector.dir/src/UFO.cpp.o"
+  "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/demos/cosmic_protector/src/collision.cpp" "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/build/demos/cosmic_protector/CMakeFiles/cosmic_protector.dir/src/collision.cpp.o"
+  "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/demos/cosmic_protector/src/cosmic_protector.cpp" "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/build/demos/cosmic_protector/CMakeFiles/cosmic_protector.dir/src/cosmic_protector.cpp.o"
+  "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/demos/cosmic_protector/src/logic.cpp" "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/build/demos/cosmic_protector/CMakeFiles/cosmic_protector.dir/src/logic.cpp.o"
+  "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/demos/cosmic_protector/src/render.cpp" "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/build/demos/cosmic_protector/CMakeFiles/cosmic_protector.dir/src/render.cpp.o"
+  "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/demos/cosmic_protector/src/sound.cpp" "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/build/demos/cosmic_protector/CMakeFiles/cosmic_protector.dir/src/sound.cpp.o"
+  "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/demos/cosmic_protector/src/wave.cpp" "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/build/demos/cosmic_protector/CMakeFiles/cosmic_protector.dir/src/wave.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/build/addons/main/CMakeFiles/allegro_main.dir/DependInfo.cmake"
+  "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/build/addons/font/CMakeFiles/allegro_font.dir/DependInfo.cmake"
+  "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/build/addons/image/CMakeFiles/allegro_image.dir/DependInfo.cmake"
+  "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/build/addons/audio/CMakeFiles/allegro_audio.dir/DependInfo.cmake"
+  "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/build/addons/acodec/CMakeFiles/allegro_acodec.dir/DependInfo.cmake"
+  "/home/noober/My Programming/Libraries/Allegro/allegro-5.0.10/build/CMakeFiles/allegro.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "include"
+  "../demos/cosmic_protector/include"
+  "../addons/main"
+  "../addons/image"
+  "../addons/font"
+  "../addons/audio"
+  "../addons/acodec"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
